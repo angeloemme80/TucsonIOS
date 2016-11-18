c859b658557675beef5d891d9effd79f72f5b52b
Angela Mac <angela@Angelas-Mac.local>
2016-11-18 23:51:21 +0100

    aaa

--- AKSwiftSlideMenu/Facebook.swift
+++ AKSwiftSlideMenu/Facebook.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class Facebook: BaseViewController {
+class Facebook: BaseViewController, FBSDKLoginButtonDelegate {
     
     
     override func viewDidLoad() {
@@ -16,9 +18,45 @@ class Facebook: BaseViewController {
         addSlideMenuButton()
         // Do any additional setup after loading the view.
         
+        if FBSDKAccessToken.current() != nil {
+            //L'utente già possiede un access token
+            self.logUserData()
+        } else {
+            let loginButton = FBSDKLoginButton()
+            loginButton.center = self.view.center
+            loginButton.readPermissions = ["email", "public_profile","user_friends"]
+            self.view.addSubview(loginButton)
+            loginButton.delegate = self
+
+        }
         
-                
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.logUserData()
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("logged in!")
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("logged out!")
+    }
+    
+    func logUserData(){
+        let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil )
+        graphRequest?.start(completionHandler: { (connection, result, error) -> Void in
+            if error != nil {
+                print(error)
+            }else {
+                print(result)
+            }
+        })
+        
+    }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
