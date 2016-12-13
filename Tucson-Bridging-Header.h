6d2dce676275c7d6150dd7ceeb2e92ec50dd0c51
Angela Mac <angela@Angelas-Mac.local>
2016-12-13 23:40:48 +0100

    f

---
+++ Tucson-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Tucson-Bridging-Header.h
+//  Tucson
+//
+//  Created by Angela Mac on 13/12/2016.
+//  Copyright © 2016 Kode. All rights reserved.
+//
+
+#ifndef Tucson_Bridging_Header_h
+#define Tucson_Bridging_Header_h
+
+#import "GMUMarkerClustering.h"
+
+#endif /* Tucson_Bridging_Header_h */
