10ff21e9d6b3b246385927a48466954bde80ad1f
Angela Mac <angela@Angelas-Mac.local>
2017-01-26 22:57:02 +0100

    a

--- AKSwiftSlideMenu/LoginSignin.swift
+++ AKSwiftSlideMenu/LoginSignin.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 import UIKit
+import Toaster
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class LoginSignin: BaseViewController {
     
@@ -42,6 +45,73 @@ class LoginSignin: BaseViewController {
         
         
     }
+    
+    @IBAction func tapLogin(_ sender: Any) {
+        //Faccio il logout se necessario altrimenti faccio il login
+        let preferences = UserDefaults.init(suiteName: self.nomePreferenceFacebook)
+        if ( preferences?.string(forKey: "accessToken") == "asGuest" ){
+            preferences?.removeObject(forKey: "accessToken")
+            preferences?.removeObject(forKey: "facebookId")
+            
+            Toast(text: NSLocalizedString("logout_performed", comment:"")).show()
+            return
+        }
+        
+        //Effettuo il logout da facebook prima di fare il login come guest
+        let loginManager = FBSDKLoginManager()
+        loginManager.logOut() // this is an instance function
+        
+        
+        let uuid = UIDevice.current.identifierForVendor!.uuidString
+        //print(uuid)
+        
+        let urlWithParams = appDelegate.urlServizio + "ID_FROM_UUID?token=asGuest&uuid=\(uuid)"
+        let myUrl = NSURL(string: urlWithParams);
+        let request = NSMutableURLRequest(url:myUrl! as URL);
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            // Check for error
+            if error != nil {
+                print("error=\(error)")
+                return
+            }
+            
+            // Print out response string
+            //let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            //print("responseString = \(responseString)")
+            // Convert server json response to NSDictionary
+            do {
+                if let convertedJsonIntoDict = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary {
+                    let idFromUuid = convertedJsonIntoDict.value(forKey: "id_from_uuid") as! NSString
+                    let preferences = UserDefaults.init(suiteName: self.nomePreferenceFacebook)
+                    preferences?.set("asGuest", forKey: "accessToken")
+                    preferences?.set(idFromUuid, forKey: "facebookId")
+                    preferences?.synchronize()
+                    DispatchQueue.main.async{
+                        Toast(text: NSLocalizedString("logged", comment:"")).show()
+                        self.openViewControllerBasedOnIdentifier("MappaVC")
+                    }
+                    
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            
+        }
+        
+        task.resume()
+        
+        
+    }
+    
+    @IBAction func tapSignin(_ sender: Any) {
+    }
+    
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
